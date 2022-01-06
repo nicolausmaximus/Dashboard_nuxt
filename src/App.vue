f4dbfccc87e7e71b3fa0542c0712c186b8a8760d
sreekar9601 <sreekarsarvepalli9601@gmail.com>
2022-01-06 21:12:41 +0530

    added to do and draggable content

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,76 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container mt-5">
+  <div class="row">
+  <div class="form-inLine">
+    <b-form-input v-model="newTask" placeholder="Add new task" @keyup.enter="add"></b-form-input> 
+    <b-button class ="ml-2" variant="primary" @click="addTask">Add</b-button>
+  </div>
+  </div>
+   
+<div class="row mt-3">
+  
+  <div class="col-md-3">
+    <div class="p-2 alert alert-secondary">
+      <h3>Backlog </h3>
+      <draggable class="list-group" :list="taskBackLog" group = "tasks">
+        <div class="list-group-item" v-for="element in taskBackLog" :key="element.name">{{element.name}}</div>
+      </draggable>
+    </div>
+  </div>
+
+  <div class="col-md-3">
+    <div class="p-2 alert alert-primary">
+      <h3>In Progress </h3>
+      <draggable class="list-group" :list="taskInProgress" group = "tasks">
+        <div class="list-group-item" v-for="element in taskInProgress" :key="element.name">{{element.name}}</div>
+      </draggable>
+    </div>
+  </div>
+
+  <div class="col-md-3">
+    <div class="p-2 alert alert-warning">
+      <h3>Done </h3>
+      <draggable class="list-group" :list="taskDone" group = "tasks">
+        <div class="list-group-item" v-for="element in taskDone" :key="element.name">{{element.name}}</div>
+      </draggable>
+    </div>
+  </div>
+</div>
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import draggable from "vuedraggable";
 export default {
-  name: 'App',
+  name: "kanban-board",
   components: {
-    HelloWorld
+    draggable
+  },
+  data(){
+    return{
+      newTask: "",
+      taskBackLog:[
+        {name:"Task A"},
+        {name:"Task B"},   
+        {name:"Task C"},  
+      ],
+      taskInProgress:[
+      ],
+      taskDone:[]
+    }
+  },
+  methods:{
+    addTask(){
+      if(this.newTask){
+        this.taskBackLog.push({name: this.newTask});
+        this.newTask = "";//Blank string to clear out the text box
+      }
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
